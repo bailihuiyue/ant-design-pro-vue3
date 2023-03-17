97b5f2fcb84524157abcfe31e37d89e75bb88617
Ones <964981348@qq.com>
2023-03-17 09:56:14 +0800

    全局错误提示

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,14 @@
 </template>
 
 <script lang="ts">
-import { getCurrentInstance, defineComponent } from 'vue'
+import { getCurrentInstance, defineComponent, onErrorCaptured, h } from 'vue'
 import zh_CN from 'ant-design-vue/es/locale/zh_CN'
 import en_US from 'ant-design-vue/lib/locale-provider/en_US'
 import { setDeviceType } from '@/utils/device'
 import LockScreen from '@/components/LockScreen/index.vue'
 import emitter from '@/utils/eventBus'
 import { useRouter } from 'vue-router'
+import { Modal } from 'ant-design-vue'
 
 export default defineComponent({
   components: { LockScreen },
@@ -26,6 +27,20 @@ export default defineComponent({
     const router = useRouter()
     emitter.once('axios_goto_login', () => {
       router.push({ name: 'login' })
+    })
+
+    //全局错误处理
+    onErrorCaptured((err, instance, info) => {
+      if (window.env !== 'localhost') {
+        // debugger
+        console.log(err, instance, info)
+        Modal.error({
+          title: 'System Error',
+          content: h('pre', err.stack),
+          class: 'errorCapturedModal',
+          width: '60%'
+        })
+      }
     })
 
     return {
