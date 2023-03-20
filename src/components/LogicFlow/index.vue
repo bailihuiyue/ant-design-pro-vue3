f544958ab31129b761b328e6fa079b77c5d5071b
Ones <964981348@qq.com>
2023-03-20 19:08:20 +0800

    Logicflow优化

--- src/components/LogicFlow/index.vue
+++ src/components/LogicFlow/index.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts" setup name="LogicFlow">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import icon from './icon/rectImg';
 import customRect from "./basic/customRect";
 import customBezierEdge from "./edge/customBezierEdge";
@@ -17,6 +17,7 @@ import '@logicflow/extension/lib/style/index.css'
 let lfRef = null
 let selectedNode = { type: '', id: '' }
 const emit = defineEmits(['parentNodes'])
+const props = defineProps(['flowData'])
 
 const clearSelectedNodeStyle = (lf) => {
   lf.setProperties(selectedNode.id, selectedNode.type === 'node' ? nodeStyle : edgeStyle)
@@ -64,7 +65,7 @@ onMounted(() => {
     // 再设置新的id
     lf.setProperties(data.id, selectedNodeStyle)
     selectedNode = { id: data.id, type: 'node' }
-    getAllParentNodes(lf, data.id, data.text.value)
+    getAllParentNodes(lf, data.id, data?.text?.value)
   });
   lf.on('edge:click', ({ data }) => {
     // 先清除之前的颜色
@@ -107,130 +108,16 @@ onMounted(() => {
   ]);
 
   // ajax 获取数据
-  lf.render({
-    "nodes": [
-      {
-        "id": "0aae5afe-6f20-49ca-98ef-30d73e345c98",
-        "type": "custom-rect",
-        "x": 588,
-        "y": 177,
-        "properties": {
-          "fill": "#E7F7FE",
-          "stroke": "#90CFFD"
-        },
-        "text": {
-          "x": 588,
-          "y": 177,
-          "value": "a"
-        }
-      },
-      {
-        "id": "0412a0e6-eb33-4b9f-895b-ec12295dcb5c",
-        "type": "custom-rect",
-        "x": 237,
-        "y": 170,
-        "properties": {
-          "fill": "#E7F7FE",
-          "stroke": "#90CFFD"
-        },
-        "text": {
-          "x": 237,
-          "y": 170,
-          "value": "b"
-        }
-      },
-      {
-        "id": "d096b7fa-0b9c-4b99-bfa8-b10f2b4dff62",
-        "type": "custom-rect",
-        "x": 932,
-        "y": 170,
-        "properties": {
-          "fill": "#94D5FD",
-          "stroke": "#40A6FE"
-        },
-        "text": {
-          "x": 932,
-          "y": 170,
-          "value": "c"
-        }
-      }
-    ],
-    "edges": [
-      {
-        "id": "f94ef84d-9c88-42c0-b2c2-6807caef249f",
-        "type": "custom-bezier",
-        "sourceNodeId": "0aae5afe-6f20-49ca-98ef-30d73e345c98",
-        "targetNodeId": "0412a0e6-eb33-4b9f-895b-ec12295dcb5c",
-        "startPoint": {
-          "x": 538,
-          "y": 177
-        },
-        "endPoint": {
-          "x": 287,
-          "y": 170
-        },
-        "properties": {},
-        "pointsList": [
-          {
-            "x": 538,
-            "y": 177
-          },
-          {
-            "x": 438,
-            "y": 177
-          },
-          {
-            "x": 387,
-            "y": 170
-          },
-          {
-            "x": 287,
-            "y": 170
-          }
-        ]
-      },
-      {
-        "id": "7aa8128e-9d06-47e5-aad3-9011ee1664c0",
-        "type": "custom-bezier",
-        "sourceNodeId": "0aae5afe-6f20-49ca-98ef-30d73e345c98",
-        "targetNodeId": "d096b7fa-0b9c-4b99-bfa8-b10f2b4dff62",
-        "startPoint": {
-          "x": 638,
-          "y": 177
-        },
-        "endPoint": {
-          "x": 882,
-          "y": 170
-        },
-        "properties": {
-          "stroke": "#AAB7C4"
-        },
-        "text": {
-          "x": 760,
-          "y": 173.5,
-          "value": "可以写文字"
-        },
-        "pointsList": [
-          {
-            "x": 638,
-            "y": 177
-          },
-          {
-            "x": 738,
-            "y": 177
-          },
-          {
-            "x": 782,
-            "y": 170
-          },
-          {
-            "x": 882,
-            "y": 170
-          }
-        ]
-      }
-    ]
-  })
+  watch(
+    () => props.flowData,
+    (newVal) => {
+      lf.render(newVal)
+    },
+    {
+      deep: true,
+      immediate: true
+    }
+  );
 })
 </script>
 
--- src/views/logicflow/index.vue
+++ src/views/logicflow/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="Logicflow">
-    <a-button type="primary" @click="onSaveFlowData">保存</a-button>
-    <LogicFlow ref="logicFlowRef" @parentNodes="onSetParentNodes" />
+    <a-alert message="一个可以显示流程图的插件,基于滴滴的logicflow,封装成vue3的版本,点击任意节点可以输出他所有的父级名称" />
+    <a-button type="primary" style="margin:10px 0" @click="onSaveFlowData">保存</a-button>
+    <LogicFlow ref="logicFlowRef" @parentNodes="onSetParentNodes" :flowData="flowData" />
   </div>
 </template>
 <script lang='ts' setup name="Logicflow">
@@ -14,6 +15,131 @@ const onSaveFlowData = () => {
 }
 const onSetParentNodes = data => {
   console.log(data)
+}
+
+const flowData = {
+  "nodes": [
+    {
+      "id": "0aae5afe-6f20-49ca-98ef-30d73e345c98",
+      "type": "custom-rect",
+      "x": 588,
+      "y": 177,
+      "properties": {
+        "fill": "#E7F7FE",
+        "stroke": "#90CFFD"
+      },
+      "text": {
+        "x": 588,
+        "y": 177,
+        "value": "a"
+      }
+    },
+    {
+      "id": "0412a0e6-eb33-4b9f-895b-ec12295dcb5c",
+      "type": "custom-rect",
+      "x": 237,
+      "y": 170,
+      "properties": {
+        "fill": "#E7F7FE",
+        "stroke": "#90CFFD"
+      },
+      "text": {
+        "x": 237,
+        "y": 170,
+        "value": "b"
+      }
+    },
+    {
+      "id": "d096b7fa-0b9c-4b99-bfa8-b10f2b4dff62",
+      "type": "custom-rect",
+      "x": 932,
+      "y": 170,
+      "properties": {
+        "fill": "#94D5FD",
+        "stroke": "#40A6FE"
+      },
+      "text": {
+        "x": 932,
+        "y": 170,
+        "value": "c"
+      }
+    }
+  ],
+  "edges": [
+    {
+      "id": "f94ef84d-9c88-42c0-b2c2-6807caef249f",
+      "type": "custom-bezier",
+      "sourceNodeId": "0aae5afe-6f20-49ca-98ef-30d73e345c98",
+      "targetNodeId": "0412a0e6-eb33-4b9f-895b-ec12295dcb5c",
+      "startPoint": {
+        "x": 538,
+        "y": 177
+      },
+      "endPoint": {
+        "x": 287,
+        "y": 170
+      },
+      "properties": {},
+      "pointsList": [
+        {
+          "x": 538,
+          "y": 177
+        },
+        {
+          "x": 438,
+          "y": 177
+        },
+        {
+          "x": 387,
+          "y": 170
+        },
+        {
+          "x": 287,
+          "y": 170
+        }
+      ]
+    },
+    {
+      "id": "7aa8128e-9d06-47e5-aad3-9011ee1664c0",
+      "type": "custom-bezier",
+      "sourceNodeId": "0aae5afe-6f20-49ca-98ef-30d73e345c98",
+      "targetNodeId": "d096b7fa-0b9c-4b99-bfa8-b10f2b4dff62",
+      "startPoint": {
+        "x": 638,
+        "y": 177
+      },
+      "endPoint": {
+        "x": 882,
+        "y": 170
+      },
+      "properties": {
+        "stroke": "#AAB7C4"
+      },
+      "text": {
+        "x": 760,
+        "y": 173.5,
+        "value": "可以写文字"
+      },
+      "pointsList": [
+        {
+          "x": 638,
+          "y": 177
+        },
+        {
+          "x": 738,
+          "y": 177
+        },
+        {
+          "x": 782,
+          "y": 170
+        },
+        {
+          "x": 882,
+          "y": 170
+        }
+      ]
+    }
+  ]
 }
 
 </script>
